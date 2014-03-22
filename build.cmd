874b60f5cdc92e6922616e16e1ab6134de841d23
davidebbo <david.ebbo@microsoft.com>
2014-03-22 10:21:46 -0700

    Run nuget restore before building

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
+nuget restore
 MSBuild.exe AzureSiteReplicator\AzureSiteReplicator.csproj /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="..\artifacts";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release;SolutionDir="."
 copy "%ProgramFiles%\IIS\Microsoft Web Deploy V3\Microsoft.Web.Deployment.dll" artifacts\bin
 copy "%ProgramFiles%\IIS\Microsoft Web Deploy V3\Microsoft.Web.Deployment.Tracing.dll" artifacts\bin
