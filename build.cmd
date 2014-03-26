cc26573aac6b7304e41badefa3ac70553376d905
davidebbo <david.ebbo@microsoft.com>
2014-03-25 20:28:28 -0700

    1.0.8 fix batch file to work with 32 bit cmd

--- build.cmd
+++ build.cmd
@@ -1,6 +1,7 @@
 nuget restore
+md artifacts\bin
 MSBuild.exe AzureSiteReplicator\AzureSiteReplicator.csproj /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="..\artifacts";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release;SolutionDir="."
-copy "%ProgramFiles%\IIS\Microsoft Web Deploy V3\Microsoft.Web.Deployment.dll" artifacts\bin
-copy "%ProgramFiles%\IIS\Microsoft Web Deploy V3\Microsoft.Web.Deployment.Tracing.dll" artifacts\bin
-copy "%ProgramFiles%\IIS\Microsoft Web Deploy V3\Microsoft.Web.Delegation.dll" artifacts\bin
+copy "%ProgramW6432%\IIS\Microsoft Web Deploy V3\Microsoft.Web.Deployment.dll" artifacts\bin
+copy "%ProgramW6432%\IIS\Microsoft Web Deploy V3\Microsoft.Web.Deployment.Tracing.dll" artifacts\bin
+copy "%ProgramW6432%\IIS\Microsoft Web Deploy V3\Microsoft.Web.Delegation.dll" artifacts\bin
 nuget pack
