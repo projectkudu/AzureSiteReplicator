3c2e5711070c22ea02cba69f702212a34a5775c9
María Inés Parnisari <maineparnisari@gmail.com>
2015-02-06 01:31:08 -0300

    Added check for null publish settings file. Edited README.

---
+++ AzureSiteReplicator.Test/HomeControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using AzureSiteReplicator.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AzureSiteReplicator.Test
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void ShouldNotAllowAddingNullPublishSettings()
+        {
+            var homeController = new HomeController();
+
+            HttpPostedFileBase file = null;
+
+            ActionResult result = homeController.Index(file);
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
